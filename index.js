f572c99670cd6d7e2889897dcfc66b224b481864
James Sartelle <james.sartelle@extra.app>
2023-08-30 23:08:25 -0500

    More logging & comments

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ let lotURLs = []
 /** @type {Record<string, string[]>} */
 let imageURLs = {}
 
+/** @type {Promise<void>[]} */
+const downloadPromises = []
+
 let downloadCount = 0,
   failedCount = 0
 
@@ -18,22 +21,26 @@ try {
 
 /* Get the links to all the lot pages */
 try {
+  /* Check for cached lot URLs */
   lotURLs = JSON.parse(
     await readFile('cache/lotURLs.json', { encoding: 'utf-8' })
   )
   console.log('Using cached lot URLs')
 } catch {
+  console.log('Getting lot URLs...')
   for (let index = 1; index <= 12; index++) {
     const document = await getDocument(
       `https://propstoreauction.com/auctions/catalog/id/377?page=${index}`
     )
 
+    /* Find all the lot links */
     /** @type {NodeListOf<HTMLAnchorElement>} */
     const links = document.querySelectorAll('a.auc-lot-link')
     const urls = Array.from(links).map((link) => link.href)
     lotURLs.push(...urls)
   }
 
+  /* Save them to the cache file */
   await writeFile('cache/lotURLs.json', JSON.stringify(lotURLs), {
     encoding: 'utf-8',
   })
@@ -41,11 +48,13 @@ try {
 
 /* Get the image links from each lot page */
 try {
+  /* Check for cached image URLs */
   imageURLs = JSON.parse(
     await readFile('cache/imageURLs.json', { encoding: 'utf-8' })
   )
   console.log('Using cached image URLs')
 } catch {
+  console.log('Getting image URLs...')
   for (const url of lotURLs) {
     /* Get the contents of the lot page */
     const document = await getDocument(url)
@@ -64,16 +73,16 @@ try {
     imageURLs[lotName] = urls
   }
 
+  /* Save them to the cache file */
   await writeFile('cache/imageURLs.json', JSON.stringify(imageURLs), {
     encoding: 'utf-8',
   })
 }
 
-const downloadPromises = []
-
 console.log('Starting download...')
 
 for (const [lotName, urls] of Object.entries(imageURLs)) {
+  /* Remove unsafe file name characters */
   const lotNameSafe = lotName.replace(/[\\/:"*?<>|]+/g, '-')
   const folderPath = `images/${lotNameSafe}`
 
@@ -85,6 +94,7 @@ for (const [lotName, urls] of Object.entries(imageURLs)) {
   }
 
   urls.forEach(async (url, index) => {
+    /* Add #- to the filename */
     const filename = `${index + 1}-${new URL(url).pathname.slice(1)}`
 
     // Check if the image is already downloaded
@@ -97,20 +107,30 @@ for (const [lotName, urls] of Object.entries(imageURLs)) {
   })
 }
 
+/* This lets the images download simultaneously */
 Promise.all(downloadPromises).then(() => {
+  /* Log when all the images have either succeeded or failed */
   console.log(`Downloaded ${downloadCount} images, ${failedCount} failed`)
 })
 
 /** @param {string} url */
 async function getDocument(url) {
+  /* Get the raw page data */
   const page = await fetch(url)
+  /* Parse it as HTML */
   const html = new JSDOM(await page.text(), { url })
   return html.window.document
 }
 
+/**
+ * @param {string} url
+ * @param {string} path
+ */
 async function saveImage(url, path) {
   try {
+    /* Get the raw image data */
     const imageData = await fetch(url)
+    /* Turn it into a buffer and save it */
     const imageBuffer = await imageData.arrayBuffer()
     await writeFile(path, Buffer.from(imageBuffer))
     downloadCount++
